7f4b084f4adb1707387e6733cec525ddadf521d3
Sebastian-ivaniczki <sivaniczki@gmail.com>
2022-12-23 15:53:03 +0100

    added post in page whith js

--- js/script.js
+++ js/script.js
@@ -24,9 +24,9 @@ const data = [
     id: '1',
     author: 'Phil Mangione',
     postDate: '02/15/2022',
-    profilePick: 'https://picsum.photos/200/300',
+    profilePick: 'https://picsum.photos/300/300',
     post: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
-    postPick: 'https://picsum.photos/200/300',
+    postPick: 'https://picsum.photos/300/300',
     likes: '83'
 },
 {
@@ -35,23 +35,23 @@ const data = [
     postDate: '01/15/2022',
     profilePick: '',
     post: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
-    postPick: 'https://picsum.photos/200/300',
+    postPick: 'https://picsum.photos/300/300',
     likes: '15'
 },
 {
     id: '3',
     author: 'Giorgio Saracino',
     postDate: '12/15/2022',
-    profilePick: 'https://picsum.photos/200/300',
+    profilePick: 'https://picsum.photos/300/300',
     post: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
-    postPick: 'https://picsum.photos/200/300',
+    postPick: 'https://picsum.photos/300/300',
     likes: '80'
 },
 {
     id: '4',
     author: 'Filippo Gaido',
     postDate: '02/15/2022',
-    profilePick: 'https://picsum.photos/200/300',
+    profilePick: 'https://picsum.photos/300/300',
     post: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
     postPick: '',
     likes: '25'
@@ -69,9 +69,9 @@ const data = [
     id: '6',
     author: 'Mario Rossi',
     postDate: '04/15/2022',
-    profilePick: 'https://picsum.photos/200/300',
+    profilePick: 'https://picsum.photos/300/300',
     post: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
-    postPick: 'https://picsum.photos/200/300',
+    postPick: 'https://picsum.photos/300/300',
     likes: '100'
 },
 ]
@@ -81,5 +81,40 @@ data.forEach(element => {
    // prendo ogni elemento dell'aray 
     const {id ,author, postDate, profilePick, post, postPick, likes} = element;
 
-    console.log(element) 
+    //stampo in pagina 
+    socialPage.innerHTML += `
+    <div id="container" class="posts-list">
+        <!-- post di esempio/template, da togliere/commentare e generare da JS -->
+        <div class="post">
+          <div class="post__header">
+            <div class="post-meta">
+              <div class="post-meta__icon">
+                <img class="profile-pic" src="${profilePick}" alt="${author}" />
+              </div>
+              <div class="post-meta__data">
+                <div class="post-meta__author">${author}</div>
+                <div class="post-meta__time">${postDate}</div>
+              </div>
+            </div>
+          </div>
+          <div class="post__text">
+            ${post}
+          </div>
+          <div class="post__image">
+            <img src="${postPick}" alt="" />
+          </div>
+          <div class="post__footer">
+            <div class="likes js-likes">
+              <div class="likes__cta">
+                <button class="like-button js-like-button" href="#" data-postid="1">
+                  <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                  <span class="like-button__label">Mi Piace</span>
+                </button>
+              </div>
+              <div class="likes__counter">Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `    
 });
